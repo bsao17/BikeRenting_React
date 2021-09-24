067853ed35031ea9ed289a70a0f9191dcb386f61
bsao17 <declic62@gmail.com>
2021-09-24 12:49:43 +0200

    init BikeRenting React

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styleHeader from "./header.module.css";
+import Carousel from "../carousel/Carousel";
+
+const Header = () => {
+    return (
+        <div>
+            <Carousel/>
+        </div>
+    );
+};
+
+export default Header;
