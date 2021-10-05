4cd2795dd89d739b7096f18cea6e9dfef4cb2bc0
bsao17 <declic62@gmail.com>
2021-10-05 12:52:10 +0200

    add credentials form

--- src/components/body/bodyComponent/Body.tsx
+++ src/components/body/bodyComponent/Body.tsx
@@ -5,16 +5,43 @@ import {useRecoilState} from "recoil";
 import {backgroundState} from "../../../atoms/background";
 
 const Body: FunctionComponent = () => {
-    const[bg] = useRecoilState(backgroundState)
+    const [bg] = useRecoilState(backgroundState)
     return (
         <div className={styleBody.container}>
             <section className={"row w-100 m-auto"}>
-                <div className={ bg ? "card card-body col-lg-4 col-md-4 col-sm m-2 bg-secondary" : "card card-body col-lg-4 col-md-4 col-sm m-2 bg-light"}>
-                    <p className={bg ? "text-center text-light h2 " : "text-center h2"} id={styleBody.fontText}>Séléctionnez votre station sur la carte</p>
+                <div
+                    className={bg ? "card card-body col-lg-4 col-md-4 col-sm m-2 bg-secondary" : "card card-body col-lg-4 col-md-4 col-sm m-2 bg-light"}>
+                    <p className={bg ? "text-center text-light h2 " : "text-center h2"}
+                       id={styleBody.fontText}>Séléctionnez votre station sur la carte</p>
                     <Map/>
                 </div>
-                <div className={ bg ? "card card-body col-lg-4 col-md-4 col-sm m-2 bg-secondary" : "card card-body col-lg-4 col-md-4 col-sm m-2 bg-light"}>
+                <div
+                    className={bg ? "card card-body col-lg-4 col-md-4 col-sm m-2 bg-secondary" : "card card-body col-lg-4 col-md-4 col-sm m-2 bg-light"}>
                     <section className={"m-2"}>
+                        <h3 className={bg ? "text-center text-light h2 " : "text-center h2"} id={styleBody.fontText}>Informations et réservation</h3>
+                        <form className={"form-control"} id={styleBody.form}>
+                            <div className={"d-flex flex-column"}>
+                                <label htmlFor="station">Station</label>
+                                <input className={"form-control"} type="text" id={"station"} name={"station"}/>
+                            </div>
+                            <div className={"d-flex flex-column"}>
+                                <label htmlFor="address">Adresse</label>
+                                <input className={"form-control"} type="text" id={"address"} name={"address"}/>
+                            </div>
+                            <div className={"d-flex flex-column"}>
+                                <label htmlFor="cycle">Vélos disponibles</label>
+                                <input className={"form-control"} type="text" id={"cycle"} name={"cycle"}/>
+                            </div>
+                            <div className={"d-flex flex-column"}>
+                                <label htmlFor="lastname">Votre Nom</label>
+                                <input className={"form-control"} type="text" id={"lastname"} name={"lastname"} required/>
+                            </div>
+                            <div className={"d-flex flex-column"}>
+                                <label htmlFor="firstname">Votre Prénom</label>
+                                <input className={"form-control"} type="text" id={"firstname"} name={"firstname"} required/>
+                            </div>
+                            <input className={"btn btn-sm btn-success mt-2 mb-1"} type="submit" value={"Réserver"}/>
+                        </form>
 
                     </section>
                 </div>
--- src/components/header/headerComponent/Header.tsx
+++ src/components/header/headerComponent/Header.tsx
@@ -23,11 +23,11 @@ const Header: FunctionComponent = (props) => {
     }
 
     useEffect(()=> {
-        requestDb()
+        requestDb().then((r)=>console.log(r))
     }, [toggleWindow])
 
     return (
-        <div className={"card card-body mx-4 bg-dark border-0"} id={styleHeader.container} >
+        <div className={"card card-body mx-2 bg-dark border-0"} id={styleHeader.container} >
             <img className={styleHeader.logo} src={"images/logo.png"} alt="Logo JCDecaux"/>
             <Carousel/>
             <h1 className={"text-light text-center mt-5"} id={styleHeader.fontText}>Louer un vélo en 3 étapes</h1>
@@ -35,14 +35,14 @@ const Header: FunctionComponent = (props) => {
                 onClick={()=>setToggleWindow(!toggleWindow)}
                 className={toggleWindow ? "btn w-25 m-auto mt-4 btn-danger rounded-pill" : "btn w-25 m-auto mt-4 btn-success"}
             >
-                {toggleWindow ? "Fermer" : "Ouvrir"}
+                {toggleWindow ? "Fermer les étapes" : "Lire les étapes"}
             </button>
             <div className={" container row mt-5 m-auto"}>
                     {articleContent.map((window: Article) => {
                         let windowElement: ReactElement = (
                             <div key ={window.id} className={"col-lg-4 col-md-6 col-sm-12"}>
                                 <div className={bg ? "card card-body my-2 bg-secondary" : "card card-body my-2"}>
-                                    <div className={ bg ? "card-header text-center text-dark fw-bold bg-light" : "card-header text-center text-dark fw-bold"}>
+                                    <div className={ bg ? "card-header bg-light text-center text-dark fw-bold " : "card-header bg-primary text-light text-center fw-bold"}>
                                         <span id={styleHeader.containerTitle} className="h4 fw-bold ">{window.title}</span>
                                     </div>
                                     <div className={ bg ? "card-text m-2 text-light" : "card-text m-2"}>
