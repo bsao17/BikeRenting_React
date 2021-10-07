34c5b937e678177be0f673d95a4b3cab83d6ca37
bsao17 <declic62@gmail.com>
2021-10-07 19:50:20 +0200

    work in progress on multiple markers from http request data

--- src/components/body/map/Map.tsx
+++ src/components/body/map/Map.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState} from 'react'
-import {GoogleMap, LoadScript, Marker} from '@react-google-maps/api';
+import React, {useState} from 'react'
+import {GoogleMap, LoadScript, Marker, MarkerProps} from '@react-google-maps/api';
 import styleMap from "./map.module.css"
 import axios from "axios";
 
@@ -13,28 +13,27 @@ const center = {
     lng: 1.444209
 };
 
+const centerMarkerOne: locationMarker[] = [
+    {
+        lat: 43.604652,
+        lng: 1.444209
+    },
+    {
+        lat: 43.624652,
+        lng: 1.444209
+    }
+]
+
 interface locationMarker {
     lat: number
     lng: number
 }
 
 function Map() {
-    const[location, setLocation] = useState<locationMarker[]>()
+    const[centerMarker, setCenterMarker] = useState(centerMarkerOne)
 
-    function requestMarker(){
-        axios.get(`https://api.jcdecaux.com/vls/v1/stations?contract=Toulouse&apiKey=${process.env.REACT_APP_JCD_API_KEY}`)
-            .then((response)=>{
-                response.data.map((r: {lat:number, lng: number}[])=> {
-                    setLocation(r)
-                    console.log(r)
-                    return location
-                })
-            })
-    }
 
-    const centerMarker = location
 
-    useEffect(requestMarker , [])
     return (
         <LoadScript
             googleMapsApiKey= {`${process.env.REACT_APP_API_KEY}`}
@@ -46,10 +45,11 @@ function Map() {
                     zoom={11}
                 >
                     { /* Child components, such as markers, info windows, etc. */ }
-                    {centerMarker!.map((C: locationMarker)=>{
-                        return (<Marker key={C.lat} position={C} icon={"https://www.icone-png.com/png/39/38961.png"}/>)
-
-                    })}
+                    {
+                        centerMarker.map((C)=> {
+                            return (<Marker position={C} icon={"https://www.icone-png.com/png/39/38981.png"}/>)
+                        })
+                    }
                     <></>
                 </GoogleMap>
             </div>
