c675ade927c146937bc8997d1028951b819083ff
bsao17 <declic62@gmail.com>
2021-10-07 11:22:11 +0200

    work in progress on multiple markers from http request data

--- src/components/body/map/Map.tsx
+++ src/components/body/map/Map.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useEffect, useState} from 'react'
 import {GoogleMap, LoadScript, Marker} from '@react-google-maps/api';
 import styleMap from "./map.module.css"
 import axios from "axios";
@@ -13,37 +13,28 @@ const center = {
     lng: 1.444209
 };
 
-const centerMarker = [
-    {
-    lat: 43.604652,
-    lng: 1.444209,
-    },
-    {
-    lat: 43.614660,
-    lng: 1.444209,
-    }
-];
-
+interface locationMarker {
+    lat: number
+    lng: number
+}
 
 function Map() {
-    const[Lat, setLat] = useState<number>()
-    const[Lng, setLng] = useState<number>()
+    const[location, setLocation] = useState<locationMarker[]>()
 
     function requestMarker(){
         axios.get(`https://api.jcdecaux.com/vls/v1/stations?contract=Toulouse&apiKey=${process.env.REACT_APP_JCD_API_KEY}`)
             .then((response)=>{
-                response.data.map((r: any)=> {
-                    return (
-                        <Marker position={r.data["position"]}
-                                icon={"https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"}/>
-                    )
+                response.data.map((r: {lat:number, lng: number}[])=> {
+                    setLocation(r)
+                    console.log(r)
+                    return location
                 })
             })
     }
 
+    const centerMarker = location
 
-    console.log(Lat, Lng)
-
+    useEffect(requestMarker , [])
     return (
         <LoadScript
             googleMapsApiKey= {`${process.env.REACT_APP_API_KEY}`}
@@ -55,7 +46,7 @@ function Map() {
                     zoom={11}
                 >
                     { /* Child components, such as markers, info windows, etc. */ }
-                    {centerMarker.map((C)=>{
+                    {centerMarker!.map((C: locationMarker)=>{
                         return (<Marker key={C.lat} position={C} icon={"https://www.icone-png.com/png/39/38961.png"}/>)
 
                     })}
