4978f047d924ec46b6ed02ec14c8498cc478c13d
bsao17 <declic62@gmail.com>
2021-10-09 12:30:44 +0200

    add fields values to stations

--- src/components/body/bodyComponent/Body.tsx
+++ src/components/body/bodyComponent/Body.tsx
@@ -1,11 +1,22 @@
-import React, {FunctionComponent} from 'react';
+import React, {FunctionComponent, useState} from 'react';
 import styleBody from "./body.module.css"
 import Map from "../map/Map";
 import {useRecoilState} from "recoil";
 import {backgroundState} from "../../../atoms/background";
 
+
 const Body: FunctionComponent = () => {
-    const [bg] = useRecoilState(backgroundState)
+    const[bg] = useRecoilState(backgroundState)
+    const[address, setAddress] = useState("")
+    const[nameStation, setNameStation] = useState("")
+    const[bicycle, setBicycle] = useState("")
+
+    const childReq = (A: string, N: string, B: string): any =>{
+        setAddress(A)
+        setNameStation(N)
+        setBicycle(B)
+    }
+
     return (
         <div className={styleBody.container}>
             <section className={"row w-100 m-auto"}>
@@ -13,7 +24,10 @@ const Body: FunctionComponent = () => {
                     className={bg ? "card card-body col-lg-4 col-md-4 col-sm m-2 bg-secondary" : "card card-body col-lg-4 col-md-4 col-sm m-2 bg-light"}>
                     <p className={bg ? "text-center text-light h2 " : "text-center h2"}
                        id={styleBody.fontText}>Séléctionnez votre station sur la carte</p>
-                    <Map/>
+
+                    {/*Child component*/}
+                    <Map updateStation={childReq} />
+
                 </div>
                 <div
                     className={bg ? "card card-body col-lg-4 col-md-4 col-sm m-2 bg-secondary" : "card card-body col-lg-4 col-md-4 col-sm m-2 bg-light"}>
@@ -22,15 +36,15 @@ const Body: FunctionComponent = () => {
                         <form className={"form-control"} id={styleBody.form}>
                             <div className={"d-flex flex-column"}>
                                 <label htmlFor="station">Station</label>
-                                <input className={"form-control"} type="text" id={"station"} name={"station"}/>
+                                <input className={"form-control"} type="text" defaultValue={nameStation} id={"station"} />
                             </div>
                             <div className={"d-flex flex-column"}>
                                 <label htmlFor="address">Adresse</label>
-                                <input className={"form-control"} type="text" id={"address"} name={"address"}/>
+                                <input className={"form-control"} type="text" id={"address"} defaultValue={address} name={"address"}/>
                             </div>
                             <div className={"d-flex flex-column"}>
                                 <label htmlFor="cycle">Vélos disponibles</label>
-                                <input className={"form-control"} type="text" id={"cycle"} name={"cycle"}/>
+                                <input className={"form-control"} type="text" id={"cycle"} defaultValue={bicycle} name={"cycle"}/>
                             </div>
                             <div className={"d-flex flex-column"}>
                                 <label htmlFor="lastname">Votre Nom</label>
--- src/components/body/map/Map.tsx
+++ src/components/body/map/Map.tsx
@@ -18,17 +18,23 @@ interface locationMarker {
     lng: number
 }
 
-function Map() {
+interface stationProps {
+    updateStation: Function
+}
+
+function Map({updateStation}: stationProps) {
     const [centerMarker, setCenterMarker] = useState<locationMarker[]>()
-    function JcdecauxRequest (){
+
+    function JcdecauxRequest() {
         axios.get(`https://api.jcdecaux.com/vls/v1/stations?contract=Toulouse&apiKey=e56f43cd9e4a4aa5260f59360a683fa28aaa4e6b`).then(
             (R) => {
                 console.log(R.data)
-                const stationsData =  R.data
+                const stationsData = R.data
                 setCenterMarker(stationsData)
             }
         )
     }
+
     useEffect(JcdecauxRequest, [])
     return (
         <LoadScript
@@ -44,24 +50,22 @@ function Map() {
                     {
                         centerMarker?.map((C) => {
                             //@ts-ignore
-                            if(C["available_bikes"] === 0){
+                            if (C["available_bikes"] === 0) {
                                 //@ts-ignore
-                                console.log(C["available_bikes"])
-                                //@ts-ignore
-                                return (<Marker key={C["address"]}
+                                return (<Marker key={ C["address"] }
                                     //@ts-ignore
-                                                position={{lat: C["position"].lat, lng: C["position"].lng}}
+                                                position={ {lat: C["position"].lat, lng: C["position"].lng} }
                                                 icon={"http://maps.google.com/mapfiles/ms/icons/red-dot.png"}/>)
                                 //@ts-ignore
-                            }else if(C["available_bikes"] <= 5){
+                            } else if (C["available_bikes"] <= 5) {
                                 //@ts-ignore
-                                return (<Marker key={C["address"]}
+                                return (<Marker onClick={() => { updateStation(C["address"], C["name"], C["available_bikes"]) }} key={C["address"]}
                                     //@ts-ignore
                                                 position={{lat: C["position"].lat, lng: C["position"].lng}}
                                                 icon={"http://maps.google.com/mapfiles/ms/icons/yellow-dot.png"}/>)
-                            } else{
+                            } else {
                                 //@ts-ignore
-                                return (<Marker key={C["address"]}
+                                return (<Marker onClick={() => { updateStation(C["address"], C["name"], C["available_bikes"]) }} key={C["address"]}
                                     //@ts-ignore
                                                 position={{lat: C["position"].lat, lng: C["position"].lng}}
                                                 icon={"http://maps.google.com/mapfiles/ms/icons/green-dot.png"}/>)
