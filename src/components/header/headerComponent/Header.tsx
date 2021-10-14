e38447fd1758e883d4c6722fff4b1c7c97b375c2
bsao17 <declic62@gmail.com>
2021-10-14 08:54:25 +0200

    refactoring useMemo to useCallback

--- src/components/header/headerComponent/Header.tsx
+++ src/components/header/headerComponent/Header.tsx
@@ -16,7 +16,7 @@ interface Article {
 const Header: FunctionComponent = (props) => {
     const [articleContent, setArticleContent] = useState([])
     const [toggleWindow, setToggleWindow] = useState<Boolean>(false)
-    const[bg] = useRecoilState(backgroundState)
+    const [bg] = useRecoilState(backgroundState)
 
     const requestHttp = useCallback(
         function requestDb() {
@@ -27,14 +27,14 @@ const Header: FunctionComponent = (props) => {
                 })
         }
         , [])
-    useEffect(()=>requestHttp(), [requestHttp])
+    useEffect(() => requestHttp(), [requestHttp])
     return (
-        <div className={bg ? styleHeader.containerDark : styleHeader.containerLight} id={styleHeader.container} >
+        <div className={bg ? styleHeader.containerDark : styleHeader.containerLight} id={styleHeader.container}>
             <img className={styleHeader.logo} src={"images/logo.png"} alt="Logo JCDecaux"/>
             <Carousel/>
             <h1 className={"text-light text-center gx-5 mt-5"} id={styleHeader.fontText}>Louer un vélo en 3 étapes</h1>
             <button
-                onClick={()=>setToggleWindow(!toggleWindow)}
+                onClick={() => setToggleWindow(!toggleWindow)}
                 className={toggleWindow ? styleHeader.closeButton : styleHeader.openButton}
             >
                 {toggleWindow ? "OFF" : "ON"}
@@ -42,21 +42,22 @@ const Header: FunctionComponent = (props) => {
             <div className={" container row g-2  m-auto"}>
 
                 {/*Mapping "article Content" for create three window elements*/}
-                    {articleContent.map((window: Article) => {
-                        let windowElement: ReactElement = (
-                            <div key ={window.id} className={"col-lg-4 col-md-6 col-sm-12"}>
-                                <div className={bg ? "card card-body mb-4 bg-secondary" : "card card-body mb-4"}>
-                                    <div className={ bg ? "card-header bg-dark text-center text-light fw-bold " : "card-header bg-light text-center fw-bold"}>
-                                        <span id={styleHeader.containerTitle} className="h4 fw-bold ">{window.title}</span>
-                                    </div>
-                                    <div className={ bg ? "card-text m-2 text-light" : "card-text m-2"}>
-                                        {toggleWindow && window.content}
-                                    </div>
+                {articleContent.map((window: Article) => {
+                    let windowElement: ReactElement = (
+                        <div key={window.id} className={"col-lg-4 col-md-6 col-sm-12"}>
+                            <div className={bg ? "card card-body mb-4 bg-secondary" : "card card-body mb-4"}>
+                                <div
+                                    className={bg ? "card-header bg-dark text-center text-light fw-bold " : "card-header bg-light text-center fw-bold"}>
+                                    <span id={styleHeader.containerTitle} className="h4 fw-bold ">{window.title}</span>
+                                </div>
+                                <div className={bg ? "card-text m-2 text-light" : "card-text m-2"}>
+                                    {toggleWindow && window.content}
                                 </div>
                             </div>
-                        )
-                        return windowElement
-                    })}
+                        </div>
+                    )
+                    return windowElement
+                })}
             </div>
         </div>
     );
